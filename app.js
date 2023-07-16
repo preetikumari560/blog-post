1d7057342bd1b4dc438b98de5b2e0d5d9c5a6248
preetikumari560 <preetigoswami525@gmail.com>
2023-07-16 20:12:45 +0530

    ui fixed

--- app.js
+++ app.js
@@ -42,8 +42,6 @@ const blogSchema = {
 const Blog = mongoose.model("blog",blogSchema)  // 'blog' which is turn out to be 'blogs' in mongoshell is a collection
 
 
-const homeStartingContent="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime perspiciatis consequatur dolorem doloribus expedita fugit quaerat dolores blanditiis, nihil illo. Ut omnis recusandae quibusdam exercitationem ratione hic odit optio nesciuntQuo doloremque repellendus in, quod ea magni incidunt possimus inventore ratione voluptatem facere eos ducimus nihil similique voluptatibus, unde blanditiis, quidem perspiciatis nisi pariatur quia voluptates nesciunt! Voluptate, quos veritatis."
-
 const aboutsContent="Welcome to my blog! I am so glad you stopped by to read my latest post. Whether you're a regular reader or new to my blog, I hope you'll find something interesting and useful here.In this blog, I will be sharing my thoughts, insights, and experiences on a wide range of topics. From technology and science to travel and lifestyle, I love exploring new subjects and sharing what I learn with others.My goal is to provide you with informative, thought-provoking content that inspires you to learn, grow, and achieve your goals. I believe that we all have the potential to do great things, and I hope my blog can be a source of inspiration and motivation for you.So, take a look around, read some of my posts, and don't hesitate to leave a comment or get in touch with me. I would love to hear from you and learn more about your interests and ideas.Thank you for visiting my blog, and I hope you enjoy your stay"
 
 const contactsContent ="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime perspiciatis consequatur dolorem doloribus expedita fugit quaerat dolores blanditiis, nihil illo. Ut omnis recusandae quibusdam exercitationem ratione hic odit optio nesciunt.Quo doloremque repellendus in, quod ea magni incidunt possimus inventore ratione voluptatem facere eos ducimus nihil similique voluptatibus, unde blanditiis, quidem perspiciatis nisi pariatur quia voluptates nesciunt! Voluptate, quos veritatis."
@@ -60,7 +58,8 @@ app.get("/",(req,res)=>
     
     if(!err)
     {
-        res.render('home',{startingContent:homeStartingContent,blogPost:posts})
+    res.render('home',{blogPost:posts})
+       
     }
     else
     {
@@ -73,13 +72,13 @@ app.get("/",(req,res)=>
 
 app.get("/contact",(req,res)=>
 {
-    res.render('contact',{contactContent:contactsContent})
+    res.render('aboutAndContact',{description:contactsContent,content:'Contact US'})
 })
 
 
 app.get("/about",(req,res)=>
 {
-    res.render('about',{aboutContent:aboutsContent})
+    res.render('aboutAndContact',{description:aboutsContent,content:'About' })
 })
 
 
@@ -116,7 +115,7 @@ app.get("/blogpost/:id",(req,res)=>{
         })
       
     
-app.post("/blogpost/:id",(req,res)=>
+app.post("/blogpost/delete/:id",(req,res)=>
 {
     const checkId = req.body.delete
        
@@ -171,9 +170,37 @@ app.post("/compose",(req,res)=>
 })
 
 
+app.get("/blogpost/edit/:id", (req, res) => {
+  const postId = req.params.id;
 
-app.listen('3000',()=>
-{console.log('server is running at port 3000')})
+  Blog.findOne({ _id: postId }, (err, post) => {
+    if (!err) {
+      res.render("edit", { post: post });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/blogpost/update/:id", (req, res) => {
+  const postId = req.params.id;
+  const updatedPost = {
+    title: req.body.postText,
+    content: req.body.postBody
+  };
+
+  Blog.updateOne({ _id: postId }, updatedPost, (err) => {
+    if (!err) {
+      res.redirect("/");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+app.listen('5000',()=>
+{console.log('server is running at port 5000')})
 
 
 
